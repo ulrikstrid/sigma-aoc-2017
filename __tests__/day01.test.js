35ebb7d9ca38254adcbabf3b6ef63b6228931c3b
Ulrik Augustsson <ulrikaugustsson@gmail.com>
2017-12-21 11:18:55 +0100

    initial setup

---
+++ __tests__/day01.test.js
@@ -0,0 +1,6 @@
+/* @flow */
+import add from "../src/day01";
+
+test("add", () => {
+  expect(add(1, 2)).toBe(3);
+});
---
+++ src/day01.js
@@ -0,0 +1,5 @@
+/* @flow */
+
+export default function add(a: number, b: number) {
+  return a + b;
+}
